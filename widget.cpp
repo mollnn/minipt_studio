58fde1ffae724a84adc0b9056de56d107a15671e
mollnn <1084918073@qq.com>
2021-05-09 22:46:42 -0700

    qopengl env

--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,24 @@
 #include "widget.h"
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-{
-}
+    : QOpenGLWidget(parent)
+{}
 
 Widget::~Widget()
-{
+{}
 
+void Widget::initializeGL()
+{
+    this->initializeOpenGLFunctions();        //初始化opengl函数
+}
+
+void Widget::resizeGL(int w, int h)
+{
+    this->glViewport(0,0,w,h);                //定义视口区域
+}
+
+void Widget::paintGL()
+{
+    this->glClearColor(0.1f,0.5f,0.7f,1.0f);  //设置清屏颜色
+    this->glClear(GL_COLOR_BUFFER_BIT);       //清除颜色缓存
 }
--- widget.h
+++ widget.h
@@ -1,15 +1,20 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
+#include <QOpenGLWidget>
+#include <QOpenGLExtraFunctions>
 
-class Widget : public QWidget
+class Widget : public QOpenGLWidget,public QOpenGLExtraFunctions
 {
     Q_OBJECT
 
 public:
     Widget(QWidget *parent = 0);
     ~Widget();
+protected:
+    virtual void initializeGL() override;
+    virtual void resizeGL(int w,int h) override;
+    virtual void paintGL() override;
 };
 
 #endif // WIDGET_H
