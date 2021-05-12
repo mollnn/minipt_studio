67e367bc8b77f1cae76bca7ce22923e3cbaeb3f0
mollnn <1084918073@qq.com>
2021-05-12 00:15:56 -0700

    feat: send cmd

---
+++ cmdedit.cpp
@@ -0,0 +1,6 @@
+#include "cmdedit.h"
+
+CmdEdit::CmdEdit()
+{
+
+}
---
+++ cmdedit.h
@@ -0,0 +1,11 @@
+#ifndef CMDEDIT_H
+#define CMDEDIT_H
+
+
+class CmdEdit : public QTextEdit
+{
+public:
+    CmdEdit();
+};
+
+#endif // CMDEDIT_H
